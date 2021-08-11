9e68e6eb52198db41c3f698617c304fc82e4fcde
esfacu <facu_2394@hotmail.com>
2021-08-11 12:20:39 -0300

    Loggin
    Se carga login con boostrap

--- js/categories.js
+++ js/categories.js
@@ -46,6 +46,7 @@ function showCategoriesList(){
         if (((minCount == undefined) || (minCount != undefined && parseInt(category.productCount) >= minCount)) &&
             ((maxCount == undefined) || (maxCount != undefined && parseInt(category.productCount) <= maxCount))){
 
+       
             htmlContentToAppend += `
             <a href="category-info.html" class="list-group-item list-group-item-action">
                 <div class="row">
