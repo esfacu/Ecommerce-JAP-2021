d2a5b1a2aba544f52fdb0ad6dfd7c1308a08ab3a
esfacu <facu_2394@hotmail.com>
2021-09-17 22:04:20 -0300

    Se agrega comentarios y calificacion estrellas

--- js/product-info.js
+++ js/product-info.js
@@ -1,7 +1,4 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-
+// trae imagenes galeria
 
 var product = {};
 
@@ -34,6 +31,8 @@ function showImagesGallery(array){
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
+//json que trae informacion producto
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
         if (resultObj.status === "ok")
@@ -63,3 +62,187 @@ document.addEventListener("DOMContentLoaded", function(e){
         }
     });
 });
+
+let relatedContainer = document.getElementById('related-products');
+
+//Grupo de variables usadas en la sección de hacer un nuevo comentario
+let commentSection = document.getElementById('comment-section');
+let commentInput = document.getElementById('comment-input');
+let commentSend = document.getElementById('comment-send');
+let starOne = document.getElementById('starOne');
+let starTwo = document.getElementById('starTwo');
+let starThree = document.getElementById('starThree');
+let starFour = document.getElementById('starFour');
+let starFive = document.getElementById('starFive');
+let starSelected;
+
+function showComments(array) {
+    for (item in array) {
+      let innerScore;
+      let productScore = array[item].score;
+      innerScore = (`<span class="fa fa-star checked"></span>`).repeat(productScore);
+      innerScore += (`<span class="fa fa-star"></span>`).repeat(5 - productScore);
+      commentSection.innerHTML += `
+          <div class="row mb-2">
+          <div class="col-8">
+            <div class="card">
+              <div class="post-heading pl-2 pt-2">
+                <div class="float-left meta">
+                  <div class="title h5">
+                    <h4 class="text-primary"><b>${array[item].user}</b></h4>
+                  </div>
+                  <h6 class="text-muted time">${array[item].dateTime}</h6>
+                  <div class="mb-1"><span class="badge badge-light class="score-container">
+                    ${innerScore}
+                    </span></div>
+                </div>
+              </div>
+              <div class="post-description pl-2 pr-2">
+                <p>${array[item].description}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+          `
+    }
+  
+  }
+
+  
+  
+  //añade un comentario
+  function addComment() {
+    let date = new Date();
+    if (commentInput.value != 0) {
+      commentSection.innerHTML += `
+      <div class="row mb-2">
+      <div class="col-8">
+        <div class="card">
+          <div class="post-heading pl-2 pt-2">
+            <div class="float-left meta">
+              <div class="title h5">
+                <h4 class="text-primary"><b>${localStorage.getItem('usuario')}</b></h4>
+              </div>
+              <h6 class="text-muted time">${date.getFullYear()+"-"+(date.getMonth() +1)+"-"+date.getDate()+" "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()}</h6>
+              <p><span class="badge badge-light" id="punctuationComment">${starSelected}</span></p>
+            </div>
+          </div>
+          <div class="post-description pl-2 pr-2">
+            <p>${commentInput.value}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+      `
+    } else {
+      alert("Debe ingresar un comentario")
+    }
+    starOne.style.color = 'black';
+    starTwo.style.color = 'black';
+    starThree.style.color = 'black';
+    starFour.style.color = 'black';
+    starFive.style.color = 'black';
+  }
+  
+  //Función para que la calificación seleccionada se muestre en pantalla
+  //al momento de hacer un nuevo comentario.
+  
+  function rateComment(){
+    
+    starOne.addEventListener('click', function () {
+     starSelected = `<span class="fa fa-star checked"></span>
+     <span class="fa fa-star"></span>
+     <span class="fa fa-star"></span>
+     <span class="fa fa-star"></span>
+     <span class="fa fa-star"></span>`;
+     this.style.color = 'rgb(189, 189, 19)'
+     starTwo.style.color = 'black';
+     starThree.style.color = 'black';
+     starFour.style.color = 'black';
+     starFive.style.color = 'black';
+   });
+   starTwo.addEventListener('click', function () {
+     starSelected = `
+     <span class="fa fa-star checked"></span>
+     <span class="fa fa-star checked"></span>
+     <span class="fa fa-star"></span>
+     <span class="fa fa-star"></span>
+     <span class="fa fa-star"></span>`;
+     starOne.style.color = 'rgb(189, 189, 19)';
+     this.style.color = 'rgb(189, 189, 19)';
+     starThree.style.color = 'black';
+     starFour.style.color = 'black';
+     starFive.style.color = 'black';
+   });
+   starThree.addEventListener('click', function () {
+     starSelected = `
+     <span class="fa fa-star checked"></span>
+     <span class="fa fa-star checked"></span>
+     <span class="fa fa-star checked"></span>
+     <span class="fa fa-star"></span>
+     <span class="fa fa-star"></span>`;
+     starOne.style.color = 'rgb(189, 189, 19)';
+     starTwo.style.color = 'rgb(189, 189, 19)';
+     this.style.color = 'rgb(189, 189, 19)';
+     starFour.style.color = 'black';
+     starFive.style.color = 'black';
+   });
+   starFour.addEventListener('click', function () {
+     starSelected = `
+     <span class="fa fa-star checked"></span>
+     <span class="fa fa-star checked"></span>
+     <span class="fa fa-star checked"></span>
+     <span class="fa fa-star checked"></span>
+     <span class="fa fa-star"></span>`;
+     starOne.style.color = 'rgb(189, 189, 19)';
+     starTwo.style.color = 'rgb(189, 189, 19)'
+     starThree.style.color = 'rgb(189, 189, 19)';
+     this.style.color = 'rgb(189, 189, 19)'
+     starFive.style.color = 'black';
+   });
+   starFive.addEventListener('click', function () {
+     starSelected = `
+     <span class="fa fa-star checked"></span>
+     <span class="fa fa-star checked"></span>
+     <span class="fa fa-star checked"></span>
+     <span class="fa fa-star checked"></span>
+     <span class="fa fa-star checked"></span>`;
+     starOne.style.color = 'rgb(189, 189, 19)';
+     starTwo.style.color = 'rgb(189, 189, 19)'
+     starThree.style.color = 'rgb(189, 189, 19)';
+     starFour.style.color = 'rgb(189, 189, 19)';
+     this.style.color = 'rgb(189, 189, 19)'
+   }); 
+   }
+  rateComment();
+  
+  
+  
+ 
+  document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
+      if (resultObj.status === "ok") {
+        productInformation = resultObj.data;
+        
+      }
+    });
+  
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (response) {
+      if (response.status === "ok") {
+        comments = response.data;
+        showComments(comments);
+      }
+    });
+    
+    commentSend.addEventListener('click', addComment);
+    commentInput.addEventListener('keypress', function (e) {
+      if (e.key === 'Enter') {
+        addComment();
+      }
+  });
+    $(function () {
+      $('.carousel').carousel({
+          interval: 1000
+      })
+    });
+  });
