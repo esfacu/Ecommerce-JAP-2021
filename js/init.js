110e98bda8f527841f97ceefba70e53390fd3944
esfacu <facu_2394@hotmail.com>
2021-10-01 08:53:59 -0300

    Cambio deslog de login a init

--- js/init.js
+++ js/init.js
@@ -57,3 +57,15 @@ function logear(){
 }
 
 logear();
+
+
+//funcion desloguear google
+  
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+localStorage.setItem("user","");
+window.location.href = 'index.html';
+}
