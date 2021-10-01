73ba23b3ee9d3a8cbd44885831e70efaec8be684
esfacu <facu_2394@hotmail.com>
2021-10-01 09:02:13 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -45,4 +45,11 @@ function onSignIn(googleUser) {
   }
 
 
-  
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  localStorage.setItem("user","");
+  window.location.href = 'index.html';
+  }
